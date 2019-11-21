396465c7b763868be7ac673702e8322c93132109
Shao-Yang Huang <shaoyang008@hotmail.com>
2019-11-21 02:09:38 -0800

    Shaders: apply BRDF lighting

--- Game Engine/Shaders/lighting_frag.glsl
+++ Game Engine/Shaders/lighting_frag.glsl
@@ -1,8 +1,35 @@
 #version 330
 
+const float pi = 3.14159f;
+
 in vec3 position;
+in vec3 normalVec, lightVec, eyeVec;
+
+uniform vec3 diffuse, specular;
+uniform float shininess;
+
+uniform vec3 Ambient;
+uniform vec3 Light;
 
 void main()
 {
-   gl_FragColor.xyz = normalize(position);
+   	vec3 Kd = diffuse;
+	vec3 Ks = specular;
+	float alpha = shininess;
+
+	vec3 N = normalize(normalVec);
+	vec3 L = normalize(lightVec);
+	vec3 V = normalize(eyeVec);
+	vec3 H = normalize(L+V);
+
+	float LN = max(dot(L,N),0.0);
+	float HN = max(dot(H,N),0.0);
+	float LH = max(dot(L,H),0.0);
+	
+	vec3  F = Ks + (vec3(1, 1, 1) - Ks) * pow((1 - LH), 5);
+	float G = 1 / (dot(L,H) * dot(L,H));
+	float D = (alpha + 2) / (2 * pi) * pow(HN, alpha);
+	if(HN == 0 && alpha == 0) discard;
+	
+	gl_FragColor.xyz = Ambient * Kd + Light * LN * (Kd / pi + F * G * D / 4);
 }
--- Game Engine/Shaders/lighting_vert.glsl
+++ Game Engine/Shaders/lighting_vert.glsl
@@ -1,18 +1,25 @@
 #version 330
 
-// uniform mat4 WorldView, WorldInverse, WorldProj, ModelTr, NormalTr;
-
 in vec3 vertex;
 in vec2 texcoord;
 in vec3 normal;
 
-uniform mat4 WorldView, WorldProj;
+uniform mat4 WorldView, WorldProj, WorldInverse;
+uniform mat4 ModelTr, NormalTr;
 
-out vec3 position;
+out vec3 normalVec, lightVec, eyeVec;
+
+uniform vec3 lightPos;
 
 void main()
 {
-	position = vertex.xyz;
-	gl_Position.w = 1.0;
-    gl_Position.xyz = vertex.xyz / 1000.0;
+	vec3 worldPos = (ModelTr * vec4(vertex, 1.0)).xyz;
+	vec3 eyePos = (WorldInverse * vec4(0, 0, 0, 1)).xyz;
+
+	normalVec = normal * mat3(NormalTr); 
+
+	eyeVec = eyePos - worldPos;
+	lightVec = lightPos - worldPos;
+
+	gl_Position = WorldProj * WorldView * ModelTr * vec4(vertex, 1.0);
 }
